2d107e10bdec0719277bb4c9fd622c7bfee6779c
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-02-07 10:36:36 +0530

    Time: 199 ms (47.87%), Space: 75.4 MB (6.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int res=0,curr=0,cntb=0,a=0,b=0;
-        for(auto c:fruits){
-            if(c==a||c==b){
-                curr+=1;
-            }else{
-                curr=cntb+1;
+        map<int,int> map;
+        int i,j;
+        for(i=0,j=0;j<fruits.size();j++){
+            map[fruits[j]]++;
+            if(map.size()>2){
+                if(--map[fruits[i]]==0){
+                    map.erase(fruits[i]);
+                }
+                i++;
             }
-            if(c==b){
-                cntb++;
-            }else{
-                cntb=1;
-            }
-            if(b!=c){
-                a=b;
-                b=c;
-            }
-            res=max(res,curr);
         }
-        return res;
+        return j-i;
     }
 };
