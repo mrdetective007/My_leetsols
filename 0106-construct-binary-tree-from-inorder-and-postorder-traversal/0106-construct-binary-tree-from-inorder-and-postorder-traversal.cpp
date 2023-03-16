cad1c4ff57a9b90d24a311af8f782069fdcb2e8b
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-03-16 20:49:13 +0530

    Time: 4 ms (99.66%), Space: 26.1 MB (78.64%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,34 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-        TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-        return create(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1);
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = size(inorder), postIdx = n-1;
+        return build(inorder, postorder, 0, n-1, postIdx);
     }
 
-    TreeNode* create(vector<int> &inorder, vector<int> &postorder, int is, int ie, int ps, int pe){
-        if(ps > pe){
-            return nullptr;
-        }
-        TreeNode* node = new TreeNode(postorder[pe]);
-        int pos;
-        for(int i = is; i <= ie; i++){
-            if(inorder[i] == node->val){
-                pos = i;
-                break;
-            }
-        }
-        node->left = create(inorder, postorder, is, pos - 1, ps, ps + pos - is - 1);
-        node->right = create(inorder, postorder, pos + 1, ie, pe - ie + pos, pe - 1);
-        return node;
+    TreeNode* build(vector<int>& in, vector<int>& post, int inStart, int inEnd, int& postIdx) {
+        if(inStart > inEnd) return nullptr;
+        TreeNode* root = new TreeNode(post[postIdx--]);
+        int inIdx = find(begin(in), end(in), root -> val) - begin(in);
+        root -> right = build(in, post, inIdx+1, inEnd, postIdx);
+        root -> left  = build(in, post, inStart, inIdx-1, postIdx);
+        return root;
     }
 };
