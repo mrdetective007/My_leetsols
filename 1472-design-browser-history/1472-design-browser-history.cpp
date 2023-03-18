ada30e40b3a0a64390426912cc70f50c34a7ec94
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-03-18 10:09:23 +0530

    Time: 234 ms (15.83%), Space: 122.4 MB (7.00%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,39 +1,33 @@
 class BrowserHistory {
-    stack<string> history, future;
-    string current;
 public:
+    stack<string> history;
+    stack<string> future;
+	
     BrowserHistory(string homepage) {
-        // 'homepage' is the first visited URL.
-        current = homepage;
+        history.push(homepage);
+        future = stack<string>();           // Reset the forward stack.
     }
-
+    
     void visit(string url) {
-        // Push 'current' in 'history' stack and mark 'url' as 'current'.
-        history.push(current);
-        current = url;
-        // We need to delete all entries from 'future' stack.
-        future = stack<string>();
+        history.push(url);
+        future = stack<string>();           // Reset the forward stack.
     }
     
     string back(int steps) {
-        // Pop elements from 'history' stack, and push elements in 'future' stack.
-        while(steps > 0 && !history.empty()) {
-            future.push(current);
-            current = history.top();
+        while(steps > 0 && history.size() > 1) { // Always keep at least one element in the stack. 
+            future.push(history.top());
             history.pop();
             steps--;
         }
-        return current;
+        return history.top();
     }
     
     string forward(int steps) {
-        // Pop elements from 'future' stack, and push elements in 'history' stack.
-        while(steps > 0 && !future.empty()) {
-            history.push(current);
-            current = future.top();
+        while(steps > 0 && future.size() > 0) {
+            history.push(future.top());
             future.pop();
             steps--;
         }
-        return current;
+        return history.top();
     }
 };
