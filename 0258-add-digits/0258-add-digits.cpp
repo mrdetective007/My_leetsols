e3c7513599d8eb6ea9803fe2f723710c81526ac6
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-04-26 12:23:50 +0530

    Time: 3 ms (56.14%), Space: 6 MB (7.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     
-    int solve(int num){
-        int ans=0;
-        while(num>0){
-            ans+=num%10;
-            num/=10;
-        }
-        return ans;
-    }
+    // int solve(int num){
+    //     int ans=0;
+    //     while(num>0){
+    //         ans+=num%10;
+    //         num/=10;
+    //     }
+    //     return ans;
+    // }
     
     int addDigits(int num) {
-        int ans=solve(num);
-        int fans=solve(ans);
-        if(fans>=10){
-            int ans1=solve(fans);
-            return ans1;
+        // int ans=solve(num);
+        // int fans=solve(ans);
+        // if(fans>=10){
+        //     int ans1=solve(fans);
+        //     return ans1;
+        // }
+        // return fans;
+        if(num==0){
+            return 0;
         }
-        return fans;
+        if(num%9==0){
+            return 9;
+        }
+        return num%9;
     }
 };
