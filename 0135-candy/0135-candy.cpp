8955dbdba6f9162193162079adde38d4830acd76
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-06-14 18:46:08 +0530

    Time: 17 ms (93.42%), Space: 18.2 MB (18.32%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> left(n,1),right(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i]=1+left[i-1];
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i]=1+right[i+1];
+            }
+        }
+        int tot=0;
+        for(int i=0;i<n;i++){
+            tot+=max(left[i],right[i]);
+        }
+        return tot;
+    }
+};
