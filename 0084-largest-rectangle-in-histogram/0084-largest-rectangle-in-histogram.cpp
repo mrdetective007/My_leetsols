1f395a925cf46fa41cc2a5cc659005835f3971af
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-07-02 12:46:34 +0530

    Time: 235 ms (40.27%), Space: 104.9 MB (5.05%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,24 +1,73 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& nums) {
-        nums.push_back(0);
-        int maxi=0;
-        stack<int> ind;
-        for(int i=0;i<nums.size();i++){
-            while(!ind.empty() && nums[ind.top()]>nums[i]){
-                int h=nums[ind.top()];
-                ind.pop();
-                int b;
-                if(ind.empty()==true){
-                    b=-1;
-                }else{
-                    b=ind.top();
-                }
-                int area=h*(i-b-1);
-                maxi=max(area,maxi);
+    
+    vector<int> sl(vector<int> heights){
+        int n=heights.size();
+        stack<pair<int,int>> s;
+        vector<int> nsl;
+        for(int i=0;i<n;i++){
+            if(s.empty()){
+                nsl.push_back(-1);
             }
-            ind.push(i);
+            else if(!s.empty() && s.top().first<=heights[i]){
+                nsl.push_back(s.top().second);
+            }else{
+                while(!s.empty() && s.top().first>heights[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    nsl.push_back(-1);
+                }else{
+                    nsl.push_back(s.top().second);
+                }
+            }
+            s.push({heights[i],i});
         }
-        return maxi;
+        return nsl;
+    }
+    
+    vector<int> sr(vector<int> heights){
+        int n=heights.size();
+        stack<pair<int,int>> s;
+        vector<int> nsr;
+        for(int i=n-1;i>=0;i--){
+            if(s.empty()){
+                nsr.push_back(n);
+            }
+            else if(!s.empty() && s.top().first<heights[i]){
+                nsr.push_back(s.top().second);
+            }else{
+                while(!s.empty() && s.top().first>=heights[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    nsr.push_back(n);
+                }else{
+                    nsr.push_back(s.top().second);
+                }
+            }
+            s.push({heights[i],i});
+        }
+        reverse(nsr.begin(),nsr.end());
+        return nsr;
+    }
+    
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int> nsl=sl(heights);
+        vector<int> nsr=sr(heights);
+        // for(auto x:nsl){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        // for(auto x:nsr){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=(nsr[i]-nsl[i]-1)*heights[i];
+        }
+        return *max_element(ans.begin(),ans.end());
     }
 };
