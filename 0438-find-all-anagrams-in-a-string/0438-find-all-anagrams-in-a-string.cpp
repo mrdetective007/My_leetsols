ca21801aec4e68c6093dbd90a08557519b3e6c94
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-07-12 19:18:39 +0530

    Time: 16 ms (70.67%), Space: 8.6 MB (53.10%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n=s.size(),m=p.size();
-        vector<int> ans;
-        if(m>n){
-            return {};
-        }
-        vector<int> a(26,0);
+        vector<int> vec(26,0),v(26,0),ans;
         for(auto x:p){
-            a[x-'a']++;
+            vec[x-'a']++;
         }
-        vector<int> b(26,0);
-        for(int i=0;i<m;i++){
-            b[s[i]-'a']++;
-        }
-        bool flag=true;
-        // for(int i=0;i<26;i++){
-        //     if(a[i]!=b[i]){
-        //         flag=false;
-        //         break;
-        //     }
-        // }
-        // if(flag){
-        //     ans.push_back(0);
-        // }
-        // flag=true;
-        int start=0,end=m-1;
-        while(end<n){
-            flag=true;
-            for(int j=0;j<26;j++){
-               if(a[j]!=b[j]){
-                   flag=false;
-                   break;
-               }
+        int i=0,j=0,k=p.size();
+        while(j<s.size()){
+            v[s[j]-'a']++;
+            if(j-i+1<k){
+                j++;
             }
-            if(flag){
-                ans.push_back(start);
-            }
-            b[s[start]-'a']--;
-            start++;
-            end++;
-            if(end!=n){
-                b[s[end]-'a']++;
+            else if(j-i+1==k){
+                bool flag=true;
+                for(int x=0;x<26;x++){
+                    if(vec[x]!=v[x]){
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag){
+                    ans.push_back(i);
+                }
+                v[s[i]-'a']--;
+                i++;
+                j++;
             }
         }
         return ans;
