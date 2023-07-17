e8a426a596fb2d0ec2e1491018f5c2d99d6fa305
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-07-17 22:30:33 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (38.39%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n=s2.size(),m=s1.size();
-        if(m>n){
-            return false;
-        }
-        vector<int> a(26,0);
+        int k=s1.size();
+        int i=0,j=0;
+        vector<int> vec(26,0),v(26,0);
         for(auto x:s1){
-            a[x-'a']++;
+            v[x-'a']++;
         }
-        vector<int> b(26,0);
-        int start=0,end=m-1;
-        for(int i=start;i<=end;i++){
-            b[s2[i]-'a']++;
-        }
-        //end++;
-        bool ans=true;
-        while(end<n){
-            ans=true;
-            for(int i=0;i<26;i++){
-                if(a[i]!=b[i]){
-                    ans=false;
-                    break;
+        while(j<s2.size()){
+            vec[s2[j]-'a']++;
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                bool flag=true;
+                for(int p=0;p<26;p++){
+                    if(v[p]!=vec[p]){
+                        flag=false;
+                        break;
+                    }
                 }
-            }
-            if(ans){
-                return true;
-            }
-            b[s2[start]-'a']--;
-            start++;
-            end++;
-            if(end!=n){
-                b[s2[end]-'a']++;
+                if(flag){
+                    return true;
+                }
+                vec[s2[i]-'a']--;
+                i++;
+                j++;
             }
         }
         return false;
