ed1b3c32d06c42c3ab46c58d61a4c42eb5c2901f
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-08-04 12:25:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (14.74%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    string getPermutation(int n, int k) {
-        string s;
-        for(int i=1;i<=n;i++){
-            s+=to_string(i);
+    
+    void solve(int n,int k,vector<int> &nums,string &ans,vector<int> &fact){
+        if(n==1){
+            ans+=to_string(nums.back());
+            return;
         }
-        do{
-            k--;
-            if(k==0){
-                break;
-            }
-        }while(next_permutation(s.begin(),s.end()));
-        return s;
+        int ind=k/fact[n-1];
+        if(k%fact[n-1]==0){
+            ind--;
+        }
+        ans+=to_string(nums[ind]);
+        nums.erase(nums.begin()+ind);
+        k-=fact[n-1]*ind;
+        solve(n-1,k,nums,ans,fact);
+    }
+    
+    string getPermutation(int n, int k) {
+        if(n==1){
+            return "1";
+        }
+        vector<int> fact={1,1,2,6,24,120,720,5040,40320,362880};
+        vector<int> nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+        string ans="";
+        solve(n,k,nums,ans,fact);
+        return ans;
     }
 };
