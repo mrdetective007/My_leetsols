2e5fcb16bc1b2decaa5c8f6be4eba23fa9f4419b
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-08-17 16:07:14 +0530

    Time: 540 ms (11.93%), Space: 212 MB (5.87%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int i=0,j=0;
-        priority_queue<pair<int,int>> pq;
+        // int i=0,j=0;
+        // priority_queue<pair<int,int>> pq;
+        // vector<int> ans;
+        // while(j<nums.size()){
+        //     pq.push({nums[j],j});
+        //     if(j-i+1<k){
+        //         j++;
+        //     }
+        //     else if(j-i+1==k){
+        //         while(pq.top().second<i){
+        //             pq.pop();
+        //         }
+        //         ans.push_back(pq.top().first);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // return ans;
         vector<int> ans;
-        while(j<nums.size()){
-            pq.push({nums[j],j});
-            if(j-i+1<k){
-                j++;
-            }
-            else if(j-i+1==k){
-                while(pq.top().second<i){
-                    pq.pop();
-                }
-                ans.push_back(pq.top().first);
-                i++;
-                j++;
-            }
+        multiset<int> s;
+        for(int i=0;i<k;i++){
+            s.insert(nums[i]);
+        }
+        ans.push_back(*s.rbegin());
+        for(int i=k;i<nums.size();i++){
+            s.erase(s.find(nums[i-k]));
+            s.insert(nums[i]);
+            ans.push_back(*s.rbegin());
         }
         return ans;
     }
