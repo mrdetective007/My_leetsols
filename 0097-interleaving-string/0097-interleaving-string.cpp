3d4e2ff27c04cea55cf7dea52df8b8260d33e65b
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-08-25 09:40:50 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (64.88%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-    
-    if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool table[s1.length()+1][s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                table[i][j] = true;
-            else if(i == 0)
-                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
-            else
-                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+        if(s3.size() != s1.size()+s2.size()){
+            return false;
         }
-        
-    return table[s1.length()][s2.length()];
-}
+        vector<vector<bool>> dp(s1.size()+1,vector<bool>(s2.size()+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=s2.size();i++){
+            dp[0][i]=dp[0][i-1] && s2[i-1]==s3[i-1];
+        }
+        for(int i=1;i<=s1.size();i++){
+            dp[i][0]=dp[i-1][0] && s1[i-1]==s3[i-1];
+        }
+        for(int i=1;i<=s1.size();i++){
+            for(int j=1;j<=s2.size();j++){
+                dp[i][j]=(dp[i-1][j] && s1[i-1]==s3[i+j-1]) || (dp[i][j-1] && s2[j-1]==s3[i+j-1]);
+            }
+        }
+        return dp[s1.size()][s2.size()];
+    }
 };
