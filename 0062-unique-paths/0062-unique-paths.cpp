45f37b14d1a2d040a37740af2505f03f84f05ef6
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-09-03 11:25:54 +0530

    Time: 4 ms (14.96%), Space: 6.6 MB (6.60%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int sum=0;
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    if(i>0){
+                        sum+=dp[i-1][j];
+                    }
+                    if(j>0){
+                        sum+=dp[i][j-1];
+                    }
+                    dp[i][j]=sum;   
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
