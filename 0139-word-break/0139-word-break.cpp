e59f9a7f0ccd50a564f33bec689969ef1425266c
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-09-20 10:47:55 +0530

    Time: 5 ms (74.00%), Space: 9.7 MB (62.03%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,20 @@
 class Solution {
-private:
-    bool wordBreak(string s, unordered_set<string> &set, vector<int> &memo, int j){
-        if(j == s.size()){
-            return true;
-        }
-        if(memo[j] != -1){
-            return memo[j];
-        }
-        for(int i=j; i<s.size(); i++){
-            if(set.count(s.substr(j, i-j+1)) && wordBreak(s, set, memo, i+1)){
-                memo[j] = true;
-                return true;
-            }
-        }
-        return memo[j] = false;
-    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int> memo(s.size(), -1);
-        unordered_set<string> set(wordDict.begin(), wordDict.end());
-        return wordBreak(s, set, memo, 0);
+        int n=s.size();
+        vector<bool> dp(n+1,false);
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(dp[j]){
+                    string word=s.substr(j,i-j);
+                    if(find(wordDict.begin(),wordDict.end(),word)!=wordDict.end()){
+                        dp[i]=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[n];
     }
 };
