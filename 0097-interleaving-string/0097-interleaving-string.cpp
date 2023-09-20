ff7e205216d2b202e337d45970f986f151e789ca
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-09-20 10:07:28 +0530

    Time: 3 ms (69.38%), Space: 6.9 MB (59.98%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.size() != s1.size()+s2.size()){
+        int n=s1.size(),m=s2.size();
+        if(n+m!=s3.size()){
             return false;
         }
-        vector<vector<bool>> dp(s1.size()+1,vector<bool>(s2.size()+1,false));
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1));
         dp[0][0]=true;
-        for(int i=1;i<=s2.size();i++){
-            dp[0][i]=dp[0][i-1] && s2[i-1]==s3[i-1];
+        for(int i=1;i<=n;i++){
+            dp[i][0]=(dp[i-1][0] && s3[i-1]==s1[i-1]);
         }
-        for(int i=1;i<=s1.size();i++){
-            dp[i][0]=dp[i-1][0] && s1[i-1]==s3[i-1];
+        for(int i=1;i<=m;i++){
+            dp[0][i]=(dp[0][i-1] && s3[i-1]==s2[i-1]);
         }
-        for(int i=1;i<=s1.size();i++){
-            for(int j=1;j<=s2.size();j++){
-                dp[i][j]=(dp[i-1][j] && s1[i-1]==s3[i+j-1]) || (dp[i][j-1] && s2[j-1]==s3[i+j-1]);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j]=(dp[i-1][j] && s3[i+j-1]==s1[i-1]) || (dp[i][j-1] && s3[i+j-1]==s2[j-1]);
             }
         }
-        return dp[s1.size()][s2.size()];
+        return dp[n][m];
     }
 };
