5c76c905dec4eed592533aaddb8518bf543002f8
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2023-11-04 10:52:30 +0530

    Time: 26 ms (6.90%), Space: 23.1 MB (59.05%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
-        int maxi=0;
-        for(auto x:left){
-            maxi=max(maxi,x);
-        }
-        for(auto x:right){
-            maxi=max(maxi,n-x);
-        }
-        return maxi;
+        int l=max_element(left.begin(),left.end())!=left.end()?*max_element(left.begin(),left.end()):0;
+        int r=min_element(right.begin(),right.end())!=right.end()?*min_element(right.begin(),right.end()):n;
+        return max(l,n-r);
     }
 };
