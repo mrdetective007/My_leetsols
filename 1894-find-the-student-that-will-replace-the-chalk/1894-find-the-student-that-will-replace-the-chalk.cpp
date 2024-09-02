482b6aba820f2e0a45c31fdeb0d560b560be3e87
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2024-09-02 09:31:48 +0530

    Time: 99 ms (43.59%), Space: 77 MB (22.40%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n=chalk.size();
+        long long sum=0;
+        for(auto x:chalk){
+            sum+=x;
+        }
+        k%=sum;
+        int i=0;
+        while(k>0){
+            if(k<chalk[i%n]){
+                break;
+            }
+            k-=chalk[i%n];
+            // cout<<k<<endl;
+            i++;
+        }
+        return i%n;
+    }
+};
