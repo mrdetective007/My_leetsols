d693677728f7c19d6a44fe0a289b444154fa1621
Jeet Shah <97144131+mrdetective007@users.noreply.github.com>
2024-09-23 07:23:32 +0530

    Time: 92 ms (75.34%), Space: 70.7 MB (69.93%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    unordered_set<string> st;
-    vector<int> mem;
-
-    int solve(string& s, int ind){ 
-        if(ind >= s.size()) return 0;
-        if(mem[ind] != -1) return mem[ind];
-        int res = 1 + solve(s, ind + 1);
-        for(int i = 1; ind + i <= s.size(); ++i){
-            string t = s.substr(ind, i);
-            if(st.find(t) != st.end()) res = min(res, solve(s, ind+i));
+    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index){
+        if (index >= s.size()) return 0;
+        if (dp[index] != -1) return dp[index]; //use the stored results
+        
+        string currStr = "";
+        int minExtra = s.size();
+        for (int cutIdx = index; cutIdx < s.size(); cutIdx++){
+            currStr.push_back(s[cutIdx]);
+            //currStr will be a string from (index to cutIdx)
+            
+            //if string not in dictionary, we have to delete as they are extra chars
+            int currExtra = (mp.count(currStr))? 0 : currStr.size();
+            int nextExtra = solve(s, mp, dp, cutIdx + 1);
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
         }
-        return mem[ind] = res;
+        return dp[index] = minExtra;
     }
-
     int minExtraChar(string s, vector<string>& dictionary) {
-        mem.resize(s.size() + 1, -1);
-        for(auto d: dictionary ) st.insert(d);
-        return solve(s, 0);
+        vector<int>dp(s.size(), -1);
+        unordered_map<string, int>mp;
+        for (string& word : dictionary) mp[word]++;
+        
+        int ans = solve(s, mp, dp, 0);
+        return ans;
+        
     }
 };
